f7e5cbe6b6333c4e979cc6fc74bd45e1a7ed50d7
maharsidyah <144704488+maharsidyah@users.noreply.github.com>
2023-11-19 09:55:30 +0700

    Add files via upload

---
+++ web_statis/UTS.js
@@ -0,0 +1,6 @@
+// Togle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika menu di klik
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
